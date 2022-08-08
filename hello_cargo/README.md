a0cae6195670101471518d21ccde735fc318b59b
Steve Kimoi <moenisteve@gmail.com>
2022-08-08 14:11:31 +0300

    using Rusts build system, Cargo

---
+++ hello_cargo/README.md
@@ -0,0 +1,2 @@
+# HELLO, CARGO! 
+Run ```cargo new < file name >``` to create a new project using cargo
---
+++ hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d
@@ -0,0 +1,5 @@
+/home/steve/Rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.rmeta: src/main.rs
+
+/home/steve/Rust/hello_cargo/target/debug/deps/hello_cargo-4c9ca12d9abbd603.d: src/main.rs
+
+src/main.rs:
